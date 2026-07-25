fd12db6e09936382fa6dc4b275d785d4eb636211
Gamezad_ <shabanabolfazl87@gmail.com>
2026-07-26 02:46:14 +0330

    initial

---
+++ src/main/java/ir/gamezad/lifepoi/LifePOI.java
@@ -0,0 +1,17 @@
+package ir.gamezad.lifepoi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LifePOI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
